8e43a8b73fe2fc81a87d7307a39cafd9592c171d
Bastiaan Quast <bquast@gmail.com>
2020-01-05 14:23:52 +0100

    test ReLU

--- tests/testthat/test_sigmoid.R
+++ tests/testthat/test_sigmoid.R
@@ -2,3 +2,4 @@ library(sigmoid)
 x = -5:5
 expect_equal(x, sigmoid(sigmoid(x), inverse=TRUE) )
 expect_equal(x, sigmoid(sigmoid(x, method='Gompertz'), method='Gompertz', inverse=TRUE) )
+expect_equal(c(0,0,0,0,0,0,1,2,3,4,5), sigmoid(x, method='ReLU') )
