3c96d90571288cf2c5888d5dca89f44736fbd343
Bastiaan Quast <bquast@gmail.com>
2020-01-05 14:26:41 +0100

    test leakyReLU

--- tests/testthat/test_sigmoid.R
+++ tests/testthat/test_sigmoid.R
@@ -3,3 +3,4 @@ x = -5:5
 expect_equal(x, sigmoid(sigmoid(x), inverse=TRUE) )
 expect_equal(x, sigmoid(sigmoid(x, method='Gompertz'), method='Gompertz', inverse=TRUE) )
 expect_equal(c(0,0,0,0,0,0,1,2,3,4,5), sigmoid(x, method='ReLU') )
+expect_equal(c(-0.05,-0.04,-0.03,-0.02,-0.01,0,1,2,3,4,5), sigmoid(x, method='leakyReLU') )
